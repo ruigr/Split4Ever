222a93b9738a1c8a385fe57e6157018f4978e00d
jtviegas <jtviegas@gmail.com>
2015-08-15 10:20:03 +0100

    tinkered with manifest file, now most of cf configs lie in there and deploy script is way simpler

--- deploy_cf_bluemix.sh
+++ deploy_cf_bluemix.sh
@@ -7,12 +7,12 @@ MONGO_SRV=mongo
 
 cf delete --f --r $APP_NAME
 sleep 16 
-cf push -b sdk-for-nodejs --no-start
-cf set-env $APP_NAME CONTEXT bluemix
+cf push --no-start
+#cf set-env $APP_NAME CONTEXT bluemix
 cf start $APP_NAME
-sleep 8
-cf bind-service $APP_NAME $MONGO_SRV
-cf restage $APP_NAME
+#sleep 8
+#cf bind-service $APP_NAME $MONGO_SRV
+#cf restage $APP_NAME
 sleep 16 
 cf logs $APP_NAME --recent
 
